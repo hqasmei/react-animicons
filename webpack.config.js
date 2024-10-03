6b2ff2ff3d84e1fb374089400cb8663130b1ee1b
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-10-02 23:38:19 -0700

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,24 @@ module.exports = {
     libraryTarget: "umd",
     library: "MyReactComponents",
     umdNamedDefine: true,
+    globalObject: "typeof self !== 'undefined' ? self : this",
   },
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
   externals: {
-    react: "react",
-    "react-dom": "react-dom",
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React",
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM",
+    },
   },
   module: {
     rules: [
